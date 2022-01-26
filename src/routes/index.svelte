029ce796f058d87f51302c784299988a46465249
stordahl <jacob@stordahl.dev>
2022-01-25 21:36:04 -0600

    init app

---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script lang=ts>
+  import { colorParsley } from 'colorparsley'
+  import { calcAPCA } from 'apca-w3'
+   let colorOne:string = ''
+   let colorTwo:string = ''
+
+   console.log(calcAPCA('#000','#fff'))
+</script>
+
+<div>
+  <div class="box" style:background-color={colorOne}></div>
+  <label for="color-one">Color One: {colorOne}</label>
+  <input type="text" name="color-one" bind:value={colorOne}>
+
+  <div class="box" style:background-color={colorTwo}></div>
+  <label for="color-two">Color Two: {colorTwo}</label>
+  <input type="text" name="color-two" bind:value={colorTwo}>
+</div>
+
+<style>
+  .box {
+    width: 150px;
+    height: 150px;
+  }
+
+</style>
